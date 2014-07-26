0260b9207cd6ea1fb41485737a7a957376374ec8
r <r@localhost>
2014-07-26 19:27:52 +0200

    problem 5 in scheme

--- problem005/5.scm
+++ problem005/5.scm
@@ -1,10 +1,9 @@
-(require (lib "1.ss" "srfi"))
-(require (lib "42.ss" "srfi"))
-;(require 'srfi-42)
-;(require 'srfi-1)
+(use-modules
+(srfi srfi-1)
+(srfi srfi-42))
 
 (define (range_lcm a)
-	(foldl lcm a (iota a 1)))
+	(fold lcm a (iota a 1)))
 
 (display (range_lcm 20))
 
