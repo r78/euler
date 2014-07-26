bee318253335d14dfb8a8186dfceee376c58357a
r <r@localhost>
2014-07-26 19:30:18 +0200

    simplified code for problem 6 in python

--- problem006/6.py
+++ problem006/6.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+
 def sum_of_squares(n):
 	return (n * (n + 1) * ((2 * n) + 1)) / 6
 
 def square_of_sum(n):
-	if (n % 2 == 0):
-		return ((n / 2) * (n + 1)) ** 2
-	else:
-		return (((n / 2) * (n + 1)) + n) ** 2
+	sum = (n / 2) * (n + 1)
+	if (n % 2 != 0):
+		sum += n
+	return sum ** 2
 
 print square_of_sum(100) - sum_of_squares(100)
