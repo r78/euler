76839637a02ca6b7b2a59fcc87f07a8aafa98cf6
r <r@localhost>
2014-08-03 17:49:51 +0200

    problem 9 in ruby

---
+++ problem009/9.rb
@@ -0,0 +1,9 @@
+(1..1000).each{|b|
+   (1..b).each{|a|
+      c = Math.sqrt(a**2 + b**2)
+      if a+b+c == 1000.0 and (a < b) and (b < c) then
+         puts a*b*c.to_i
+         exit
+      end
+   }
+}
