db39fe901ae5f49e265dacd06a889cf888a6a7ef
r <r@localhost>
2014-08-17 14:09:26 +0200

    clean code using helper functions

--- problem012/12.rkt
+++ problem012/12.rkt
@@ -1,6 +1,18 @@
 #lang racket/base
 (require srfi/1)
 
+;add 1 to the list of exponents if 1 is a factor of the current triangle number
+(define (append-one? num exponents)
+    (cond
+        ((= num 1) exponents)
+        (else (cons 1 exponents))))
+
+; compute the number of divisors using the Tau function
+(define (tau exponents)
+    (cond
+      ((null? exponents) 2)
+      (else (fold (lambda (e acc) (* acc (+ e 1))) 1 exponents))))
+
 ; get the number of divisors of a number
 (define (count_divisors num)
     (let loop ((i 2) (num num) (exponents (list)))
@@ -11,14 +23,8 @@
                                     (else (cond
                                             ((> p 0) (loop (+ i 1) num2 (cons p exponents)))
                                             (else (loop (+ i 1) num2 exponents)))))))
-            (else
-                (let ((exponents
-                       (cond
-                         ((= num 1) exponents)
-                         (else (cons 1 exponents)))))
-                    (cond
-                      ((null? exponents) 2)
-                      (else (fold (lambda (e acc) (* acc (+ e 1))) 1 exponents))))))))
+            (else (let ((exponents (append-one? num exponents)))
+                    (tau exponents))))))
 
 (define (solve)
     (let loop ((i 1) (triangle 1))
